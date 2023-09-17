75a9f0f600dca53514f46131f22836d514c70b00
SepehrYari <91702741+SepehrYari@users.noreply.github.com>
2023-09-16 17:55:17 -0700

    S_Yari_Unit2 Lesson B

--- Assets/Script/PlayerControls.cs
+++ Assets/Script/PlayerControls.cs
@@ -7,7 +7,7 @@ public class PlayerControls : MonoBehaviour
     public float HorizontalMovement;
     public float Speed = 10.0f;
     public float Xrange = 20.0f;
-
+    public GameObject ProjectilePrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -29,6 +29,12 @@ public class PlayerControls : MonoBehaviour
         if (transform.position.x < -Xrange)
         {
             transform.position = new Vector3(-Xrange, transform.position.y, transform.position.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("space bar was pressed");
+            Instantiate(ProjectilePrefab, transform.position, ProjectilePrefab.transform.rotation);
         }
 
 

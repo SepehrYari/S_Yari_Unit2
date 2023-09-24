3736ed3d77189694c70eae0734a17e6f07cf29ef
SepehrYari <91702741+SepehrYari@users.noreply.github.com>
2023-09-23 20:32:56 -0700

    Unit 2 Lesson D

--- Assets/Script/DestroyOutOfBounds.cs
+++ Assets/Script/DestroyOutOfBounds.cs
@@ -18,14 +18,15 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-    if(transform.position.z > topOfScene)
+        if(transform.position.z > topOfScene)
         {
             Destroy(gameObject);
         }
 
-    else if (transform.position.z < BottomOfScene)
+        else if (transform.position.z < BottomOfScene)
         {
             Destroy(gameObject);
+            Debug.Log("Game Over!");
         }
 
         
--- Assets/Script/SpawnManager.cs
+++ Assets/Script/SpawnManager.cs
@@ -10,19 +10,24 @@ public class SpawnManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", 3.0f, 1.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            float randxPos = Random.Range(-xPosRange, xPosRange);
-            int AnimalPrefabsIndex = Random.Range(0, AnimalPrefabs.Length);
-            Vector3 randPOS = new Vector3(randxPos, 0, 21);
-            Instantiate(AnimalPrefabs[AnimalPrefabsIndex], new Vector3(randxPos, 0, 21), AnimalPrefabs[AnimalPrefabsIndex].transform.rotation);
-        }
+        
         
     }
+
+    void SpawnRandomAnimal()
+    {
+        float randxPos = Random.Range(-xPosRange, xPosRange);
+        int AnimalPrefabsIndex = Random.Range(0, AnimalPrefabs.Length);
+        Vector3 randPOS = new Vector3(randxPos, 0, 21);
+        Instantiate(AnimalPrefabs[AnimalPrefabsIndex], new Vector3(randxPos, 0, 21), AnimalPrefabs[AnimalPrefabsIndex].transform.rotation);
+    }
+
+
+
 }
